9e6bf30c6c6a7f59dbe4c512ce8d7539c35fbd5d
AkashSingh1141144 <AkashSingh.Webdeveloper@gmail.com>
2024-04-19 13:01:41 +0530

    Creating React App using Vite

---
+++ React-using-vite/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import CreatingVite from './Components/CreatingVite'
+
+
+function App() {
+
+  return (
+    <>
+    <h1>Hello My Dear Friends </h1>
+    <CreatingVite />
+    </>
+  )
+}
+
+export default App
---
+++ React-using-vite/src/Components/CreatingVite.jsx
@@ -0,0 +1,9 @@
+function CreatingVite() {
+    return (
+		<>
+			<h1>I'm Akash singh </h1>
+		</>
+	)
+}
+
+export default CreatingVite;
